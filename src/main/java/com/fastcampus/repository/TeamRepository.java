5c741d039f8e97d84faee0e90a30d986edcc0b77
YongHo Shin <gdtknight@naver.com>
2023-06-28 00:54:21 +0900

    TeamRepository TeamDao @Autowired 추가

--- src/main/java/com/fastcampus/repository/TeamRepository.java
+++ src/main/java/com/fastcampus/repository/TeamRepository.java
@@ -2,11 +2,18 @@ package com.fastcampus.repository;
 
 import com.fastcampus.dao.TeamDao;
 import com.fastcampus.model.Team;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TeamRepository {
+  private final TeamDao teamDao;
+
+  public TeamRepository(@Autowired TeamDao teamDao) {
+    this.teamDao = teamDao;
+  }
+
   public void save(Team team) {
-    TeamDao.insertTeam(team.getStadiumId(), team.getName());
+    teamDao.insertTeam(team.getStadiumId(), team.getName());
   }
 }
